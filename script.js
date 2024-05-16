f577df78c72180d250a329b2b3a61da61d046e6b
Freddy Brown <absgbrown@gmail.com>
2024-05-16 14:25:18 -0500

    choice boxes work + more rounds available

--- script.js
+++ script.js
@@ -10,24 +10,41 @@
 // where the text will change to the next line of dialogue after the player presses the space bar. Finally, the project also contains
 // classes, which are used to customize the sprites, such as the alien and the furniture.
 
+// Variables to store the sprites
 let hotDate;
 let bg;
 let title;
 let tbl;
 let chair;
 let glasses;
+// For storing the players name
 let pName;
+// Used to check whether the player has correctly inputted their name
 let correct = false;
+// The amount of affection points the player has earned
 let affection = 0;
-let dial;
+// Stores the various alien dialogue
+let introDial;
+let queDial;
+let yayDial;
+let nayDial;
+// Variables for the choices in the choice box
 let optionA;
 let optionB;
+// Preloaded image variables
 let choBo;
 let romPartner;
 let recliner;
 let foodHolder;
 let cup;
 let talkBox;
+// Variables that check to see if the round is done
+let round1Done = false;
+let round2Done = false;
+let round3Done = false;
+let round4Done = false;
+let round5Done = false;
+let finalRoundDone = false;
 
 gameStart();
 
@@ -42,59 +59,98 @@ function gameStart() {
   }
 }
 
+// Intro dialogue
 let dialogue = [
   `Hey, you're ${pName}, right?`,
   `Huh… I gotta say, you're a lot more… dehydrated… than you looked in your pictures.`,
   `Not that it matters or anything, I'm sure that's common for you humans.`,
   `I'm Philix, by the way. Though, I guess you already knew that, right? 
   It's nice to finally meet you in person and not planets and planets away, haha!`,
+];
+
+// The different questions the alien asks the player
+let questionDia = [
   `So, what do you wanna get for appetizers?`,
-  ``,
-  `Wow, I think you're my dream lover… You're so perfect in every way…`,
+  `I gotta ask... what made you expand out to the cosmos for dating?`,
+  `What're you getting for the main course?`,
+  `So what do you do for fun?`,
+  `Dessert time! What do you think we should get?`,
+  `WHOA!!! THIS BILL IS OUTRAGEOUS!!! Ugh, don't worry, I'll pay for it.`,
+];
+
+// Dialogue the player gets if the alien approves their choices
+let approvalDia = [
+  `I order those all the time when I go here! I totally agree with you.`,
+  `Well aren't you adorable?`,
+  `Whoa, that looks delicious! Mind if I take a bite? Or two bites? Or three?`,
+  `Ooh, I like them a little crazy...`,
+  `It's like you read my mind! Can humans read minds?`,
+  `Yo, is this a forgery?! You're so smart!`,
+];
+
+// Dialogue the player gets if the alien disapproves their choices
+let disapprovalDia = [
+  `I think I'm actually gonna save room for the main course...`,
+  `That's kind of depressing...`,
+  `Oh... that looks... interesting...`,
+  `Oh. That's... cool... I guess...`,
+  `You can have it, I'm actually really full. And allergic.
+  And it has gluten and I can't have that either.`,
+  `Shit... I'm totally broke now...`,
+];
+
+// Dialogue if the player selects all correct choices
+let marriageDia = [
+  `What a wonderful date, ${pName}! This is probably the greatest
+  date I've ever been on!`,
+  `In fact, I think you're my dream lover! You're so perfect in every way…`,
   `I- Let's get married. Right here, right now.`,
   `${pName}... I love you.`,
   `I have never met anyone in my life who has made me feel the way you do, 
   and I really, truly think that you are the one.`,
   `Now, let's ride off into the sunset and have a million children together.`,
-  ``,
+];
+
+// Dialogue if the player selects mostly correct choices
+let lovinDia = [
+  `That was a nice date, I really enjoyed getting to know you, ${pName}!`,
+  `You're really interesting and pretty cute for a human, even if
+  there are a few things I think you could improve on...`,
+  `I feel really comfortable with you, and I can definitely see us going further 
+  in this little relationship of ours if you'd like.`,
+  `Do you… wanna come over to my place? Maybe we could spend a little more time together…`,
+];
+
+// Dialogue if the player selects mostly incorrect choices
+let rejectDia = [
+  `Well, that was quite the date huh, ${pName}?`,
+  `Listen, I'm sure you're a lovely, lovely, very dehydrated person.`,
+  `You're just… not the one for me.`,
+  `I appreciate you taking me out tonight, but I think this is the farthest 
+  I'm willing to take us.`,
+  `Goodbye, ${pName}.`,
+];
+
+// Dialogue of the player selects all incorrect choices
+let deathDia = [
+  `I'm gonna be completely honest when I say this; this is the absolute
+  WORST date I have ever been on in my life!`,
   `You are the most vile, disgusting, horrendous, dehydrated organism to ever exist 
   not only on this planet or your own, but to ever be spat out by the cosmos.`,
   `You shame not only your birth giver, but your entire bloodline and entire race.`,
   `I have never met anyone in my life who has made me feel the way you do, 
   and that's not a compliment.`,
-  `In fact, you don't even deserve to live on any planet!`,
+  `In fact, you don't deserve to live on any planet at all!`,
   `It's time to die, ${pName}.`,
 ];
 
-let marriageDia = [];
-
-let lovinDia = [
-  `You know, I really enjoyed the night with you, ${pName}.`,
-  `I feel really comfortable with you, and I can see us going further in this little relationship of ours.`,
-  `Do you… wanna come over to my place? Maybe we could spend a little more time together…`,
-];
-
-let rejectDia = [
-  `Listen, I'm sure you're a lovely, lovely, very dehydrated person.`,
-  `You're just… not the one for me.`,
-  `I appreciate you taking me out tonight, but I think this is the farthest I'm willing to take us.`,
-  `Goodbye, ${pName}.`,
-];
-
-let deathDia = [
-  `You are the most vile, disgusting, horrendous, dehydrated organism to ever exist not only on this planet or your own, but to ever be spat out by the cosmos.`,
-  `You shame not only your birth giver, but your entire bloodline and entire race.`,
-  `I have never met anyone in my life who has made me feel the way you do, and that's not a compliment.`,
-  `In fact, you don't even deserve to live on any planet!`,
-  `It's time to die, ${pName}.`,
-];
-
+// Defines the alien's sprite
 class Alien {
   x = width / 2;
   y = 300;
   constructor() {
-    this.hotDate = new Sprite(this.x, this.y, "static");
     // Creates the sprite and designates where it goes
+    this.hotDate = new Sprite(this.x, this.y, "static");
     this.hotDate.img = "Lover Sprites/hottie neutral.png";
     this.hotDate.layer = 3;
   }
@@ -142,6 +198,8 @@ function choiceBox(customY, customC, opinion) {
   const choiceSprite = new Sprite(340, customY, "static");
   choiceSprite.choices = customC;
   choiceSprite.img = choBo;
+  choiceSprite.width = choBo.width;
+  choiceSprite.height = choBo.height;
   choiceSprite.draw = function () {
     // Create the sprite
     image(choiceSprite.img, choiceSprite.x, 0);
@@ -165,6 +223,7 @@ class Chair {
   x = width / 2;
   y = 480;
   constructor() {
+    // Creates the sprite and designates where it goes
     this.chair = new Sprite(this.x, this.y, "static");
     this.chair.img = "Background and objects/chair.png";
     this.chair.layer = 1;
@@ -175,6 +234,7 @@ class Tbl {
   x = width / 2;
   y = 665;
   constructor() {
+    // Creates the sprite and designates where it goes
     this.tbl = new Sprite(this.x, this.y, "static");
     this.tbl.layer = 2;
     this.tbl.img = "Background and objects/table.png";
@@ -185,13 +245,14 @@ class Glasses {
   x = 600;
   y = 620;
   constructor() {
+    // Creates the sprite and designates where it goes
     this.glasses = new Sprite(this.x, this.y, "static");
     this.glasses.img = "Background and objects/glasses.png";
     this.glasses.layer = 4;
   }
 }
 
-// Makes sure the background is loaded in time
+// Makes sure the images are loaded in time
 function preload() {
   title = loadImage("ph title.png");
   bg = loadImage("Background and objects/background.png");
@@ -217,133 +278,246 @@ function setup() {
 // Starts the date with alien
 function dateStart() {
   // Create a dialogue box
-  dial = dialogueBox(340, 280, dialogue);
+  introDial = dialogueBox(340, 280, dialogue);
+}
+
+let loveDial;
+
+function marriageEnd() {
+  finalRoundDone = false;
+  lover.happy();
+  loveDial = dialogueBox(340, 280, marriageDia);
+}
+
+let likeDial;
+
+function lovinEnd() {
+  finalRoundDone = false;
+  lover.flirty();
+  likeDial = dialogueBox(340, 280, lovinDia);
+}
+
+let dislikeDial;
+
+function rejectEnd() {
+  finalRoundDone = false;
+  lover.disgust();
+  dislikeDial = dialogueBox(340, 280, rejectDia);
+}
+
+let hateDial;
+
+function deathEnd() {
+  finalRoundDone = false;
+  lover.angry();
+  hateDial = dialogueBox(340, 280, deathDia);
 }
 
 function appetizers() {
+  lover.neutral();
+  queDial = dialogueBox(340, 280, questionDia);
   // Good choice
   optionA = choiceBox(150, "Yum'merz", function () {
     affection++;
-    console.log("W");
     lover.happy();
+    // Gets rid of both choice boxes after a choice is picked so the player can't press them again
+    optionA.remove();
+    optionB.remove();
+    yayDial = dialogueBox(340, 280, approvalDia);
+    yayDial.index = 0;
+    // Allows the next function to call after pressing the space bar
+    round1Done = true;
   });
   // Bad choice
   optionB = choiceBox(350, "Not'Chos", function () {
     affection--;
-    console.log("L");
     lover.disgust();
+    // Gets rid of both choice boxes after a choice is picked so the player can't press them again
+    optionA.remove();
+    optionB.remove();
+    nayDial = dialogueBox(340, 280, disapprovalDia);
+    nayDial.index = 0;
+    // Allows the next function to call after pressing the space bar
+    round1Done = true;
   });
 }
 
 function whyDate() {
-  dialogueBox(
-    340,
-    280,
-    "I gotta ask... what made you expand out to the cosmos for dating?"
-  );
+  lover.neutral();
+  round1Done = false;
+  queDial = dialogueBox(340, 280, questionDia);
+  queDial.index = 1;
   // Good choice
   optionA = choiceBox(150, "Aliens are hot", function () {
     affection++;
     lover.flirty();
-    message = "Well aren't you adorable?";
+    optionA.remove();
+    optionB.remove();
+    yayDial = dialogueBox(340, 280, approvalDia);
+    yayDial.index = 1;
+    round2Done = true;
   });
-
   // Bad choice
-  optionB = new choiceBox(350, "Nobody on Earth wanted me", function () {
+  optionB = choiceBox(350, "Nobody on Earth wanted me", function () {
     affection--;
     lover.disgust();
-    message = "That's kind of depressing...";
+    optionA.remove();
+    optionB.remove();
+    nayDial = dialogueBox(340, 280, disapprovalDia);
+    nayDial.index = 1;
+    round2Done = true;
   });
 }
 
 function mainCourse() {
+  lover.neutral();
+  round2Done = false;
   // Ask the player which meal they want
-  dialogueBox(340, 280, "What're you getting for your main course?");
+  queDial = dialogueBox(340, 280, questionDia);
+  queDial.index = 2;
   // Good choice
   optionA = choiceBox(150, "Elusive Steak", function () {
     affection++;
     lover.happy();
-    message =
-      "Whoa, that looks delicious! Mind if I take a bite? Or two bites? Or three?";
+    optionA.remove();
+    optionB.remove();
+    yayDial = dialogueBox(340, 280, approvalDia);
+    yayDial.index = 2;
+    round3Done = true;
   });
   // Bad choice
   optionB = choiceBox(350, "Vegan Meat Lover's Meatball Supreme", function () {
     affection--;
     lover.disgust();
-    message = "Oh... that looks... interesting...";
+    optionA.remove();
+    optionB.remove();
+    nayDial = dialogueBox(340, 280, disapprovalDia);
+    nayDial.index = 2;
+    round3Done = true;
   });
 }
 
 function hobbies() {
-  dialogueBox(340, 280, "So what do you do for fun?");
+  lover.neutral();
+  round3Done = false;
+  queDial = dialogueBox(340, 280, questionDia);
+  queDial.index = 3;
   // Bad choice
   optionA = choiceBox(150, "Gaming", function () {
     affection--;
     lover.disgust();
-    message = "Oh. That's... cool... I guess...";
+    optionA.remove();
+    optionB.remove();
+    nayDial = dialogueBox(340, 280, disapprovalDia);
+    nayDial.index = 3;
+    round4Done = true;
   });
   // Good choice
   optionB = choiceBox(350, "Kill people with rocks", function () {
     affection++;
-    message = "Damn... you must be pretty strong, huh?";
+    lover.flirty();
+    optionA.remove();
+    optionB.remove();
+    yayDial = dialogueBox(340, 280, approvalDia);
+    yayDial.index = 3;
+    round4Done = true;
   });
 }
 
 function dessert() {
+  lover.neutral();
+  round4Done = false;
   // Ask the player which dessert they want
-  dialogueBox(340, 280, "Dessert time! What do you think we should get?");
+  queDial = dialogueBox(340, 280, questionDia);
+  queDial.index = 4;
   // Good choice
   optionA = choiceBox(150, "Scorching Lava Cake", function () {
     affection++;
     lover.happy();
-    message = "It's like you read my mind! Can humans read minds?";
+    optionA.remove();
+    optionB.remove();
+    yayDial = dialogueBox(340, 280, approvalDia);
+    yayDial.index = 4;
+    round5Done = true;
   });
   // Bad choice
   optionB = choiceBox(350, "Astronaut Ice Cream", function () {
     affection--;
     lover.disgust();
-    message =
-      "You can have it, I'm actually really full. And allergic. And it has gluten and I can't have that either.";
+    optionA.remove();
+    optionB.remove();
+    nayDial = dialogueBox(340, 280, disapprovalDia);
+    nayDial.index = 4;
+    round5Done = true;
   });
 }
 
 function theBill() {
-  dialogueBox(
-    340,
-    280,
-    "WHOA!!! THIS BILL IS OUTRAGEOUS!!! Ugh, don't worry, I'll pay for it."
-  );
+  lover.shocked();
+  round5Done = false;
+  queDial = dialogueBox(340, 280, questionDia);
+  queDial.index = 5;
   // Bad choice
   optionA = choiceBox(150, "Let them pay for it", function () {
     affection--;
     lover.angry();
-    message = "Shit... I'm totally broke now...";
+    optionA.remove();
+    optionB.remove();
+    nayDial = dialogueBox(340, 280, disapprovalDia);
+    nayDial.index = 5;
+    finalRoundDone = true;
   });
   // Good choice
   optionB = choiceBox(350, "Give them counterfeit money", function () {
     affection++;
     lover.happy();
-    message = "Yo, is this a forgery?! You're so smart!";
+    optionA.remove();
+    optionB.remove();
+    yayDial = dialogueBox(340, 280, approvalDia);
+    yayDial.index = 5;
+    finalRoundDone = true;
   });
-}
-
-function goodEnd() {
-  lover.happy();
-  dial = dialogueBox(340, 280, marriageDia);
-}
-
-function badEnd() {
-  lover.angry();
-  dial = dialogueBox(340, 280, deathDia);
 }
 
 function keyPressed() {
   // When the space bar is pressed, it moves on the to next dialogue
   if (key == " ") {
-    dial.index++;
+    introDial.index++;
     // When the last bit of dialogue is reached, it moves on to the next function
-    if (dial.index === 5) {
+    if (introDial.index === 5) {
       appetizers();
+    }
+    // Calls the next function once the previous round is completed
+    if (round1Done === true) {
+      whyDate();
+    }
+    if (round2Done === true) {
+      mainCourse();
+    }
+    if (round3Done === true) {
+      hobbies();
+    }
+    if (round4Done === true) {
+      dessert();
+    }
+    if (round5Done === true) {
+      theBill();
+    }
+    // Ending if the player reaches max affection points
+    if (finalRoundDone === true && affection === 6) {
+      marriageEnd();
+    }
+    // Ending if the player reaches high affection points
+    if (finalRoundDone === true && affection < 6 && affection >= 3) {
+      lovinEnd();
+    }
+    // Ending if the player reaches low affection points
+    if (finalRoundDone === true && affection < 3 && affection >= -5) {
+      rejectEnd();
+    }
+    // Ending if the player reaches min affection points
+    if (finalRoundDone === true && affection === -6) {
+      deathEnd();
     }
   }
 }
